eff076ebacc02fd3ca67905cf813f8ddc9c0256b
Lucas G <g@11xx.org>
2023-06-02 08:50:55 -0300

    Create README.org with tangling
    - Moved source files to README.org
    - Changed CHANGELOG.org to include section from README.org

--- Main.hs
+++ Main.hs
@@ -1,4 +1,23 @@
-module Main where
+import Network.MPD
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  s <- withMPD status
+  case s of
+    Left err -> putStrLn $ "ERROR: " ++ show err
+    Right s' -> do
+      printState (stState s')
+      printTime s'
+
+printTime :: Status -> IO ()
+printTime s =
+  case stTime s of
+    Just (elapsed, total) -> putStrLn $ "Time: \"" ++ show elapsed ++ "/" ++ show total ++ "\""
+    Nothing -> putStrLn "Time information not available"
+
+printState s =
+  case s of
+    Playing -> p "State: Playing"
+    Paused -> p "State: Paused"
+    Stopped -> p "State: Stopped"
+    where
+      p = putStrLn
